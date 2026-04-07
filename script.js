2497a64d67a188c5a6f4a2bd55f458ab6183a1be
AJUAKSHI <akshithk2310@gmail.com>
2026-04-07 23:55:43 +0530

    task added , marked and deleted into pages

--- script.js
+++ script.js
@@ -4,5 +4,35 @@ const taskList = document.getElementById('task-list')
 
 addBtn.addEventListener('click',function(){
     const taskText = taskInput.value
-    console.log(taskText)
+    
+    //don't add empty tasks
+    if (taskText === '') return
+
+    //create a new list
+    const li = document.createElement('li')
+    li.classList.add('task-item')   
+
+    //put html inside it
+    li.innerHTML = `
+        <span>${taskText}</span>
+        <button class="delete-btn">X</button>    
+    `
+
+    //add it to the list
+    taskList.appendChild(li)
+
+    //clear the input value
+    taskInput.value = ''
+
+    //delete button logic
+    const deleteBtn = li.querySelector('.delete-btn')
+    deleteBtn.addEventListener('click',function(){
+        li.remove()
+    })
+
+    //click task to mark done
+    const span = li.querySelector('span')
+    span.addEventListener('click',function(){
+        li.classList.toggle('done')
+    })
 })
